9b287b1a950516a5e0f72e8d2d6d21b6ea7266a6
Miron Alexandru <mironalex@hotmail.com>
2024-10-24 22:17:07 +0300

    Make the license text in About Form more readable

--- RainWorldSaveEditor/Forms/AboutForm.cs
+++ RainWorldSaveEditor/Forms/AboutForm.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace RainWorldSaveEditor.Forms
 {
@@ -11,7 +12,9 @@ namespace RainWorldSaveEditor.Forms
 
         private void AboutForm_Load(object sender, EventArgs e)
         {
-
+            // Makes it somewhat more readable without having to edit the resource text
+            licenseTextBox.Text = LineEndingsRegex().Replace(licenseTextBox.Text, " ");
+            licenseTextBox.Text = TripleLineRegex().Replace(licenseTextBox.Text, Environment.NewLine + Environment.NewLine);
         }
 
         private void githubLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
@@ -19,5 +22,11 @@ namespace RainWorldSaveEditor.Forms
             Process.Start(new ProcessStartInfo() { UseShellExecute = true, FileName = "https://github.com/Vultumast/RainWorldSaveEditor" });
             githubLinkLabel.LinkVisited = true;
         }
+
+        [GeneratedRegex("(?<=\\S)(\\r\\n|\\n)(?=\\S)")]
+        private static partial Regex LineEndingsRegex();
+
+        [GeneratedRegex("(\\r\\n|\\n)(\\r\\n|\\n)(\\r\\n|\\n)")]
+        private static partial Regex TripleLineRegex();
     }
 }
